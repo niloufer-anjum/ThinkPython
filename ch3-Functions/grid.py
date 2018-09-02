44be4eebcda3b865b70dcbee73e200e4955460aa
Niloufer Anjum <niloufer@8c85906470f2.ant.amazon.com>
2018-09-02 16:22:45 +0530

    Reworked grid.py

--- ch3-Functions/grid.py
+++ ch3-Functions/grid.py
@@ -1,5 +1,17 @@
 from __future__ import print_function
 
+def plus(end):
+    print('+', end=end)
+
+def minus():
+    print('- ', end=' ')
+
+def space():
+    print('  ', end=' ')
+
+def pipe(end):
+    print('|', end=end)
+
 def do_twice(f):
     f()
     f()
@@ -8,25 +20,46 @@ def do_four(f):
     do_twice(f)
     do_twice(f)
 
+def do_twice2(f, g, h):
+    f()
+    g(h)
+    f()
+    g(h)
 
-def print_beam():
-    print('+ - - - -', end=' ')
+def beam():
+    return(plus('  '), do_four(minus))
 
-def print_post():
-    print('|        ', end=' ')
+def beam2():
+    do_twice(beam)
+    plus('\n')
 
-def print_beams():
-    do_twice(print_beam)
-    print('+')
+def beam4():
+    do_four(beam)
+    plus('\n')
 
-def print_posts():
-    do_twice(print_post)
-    print('|')
+def post():
+    return(pipe('  '), do_four(space))
 
-def print_row():
-    print_beams()
-    do_four(print_posts)
+def post2():
+    do_twice(post)
+    pipe('\n')
 
-def print_grid():
-    do_twice(print_row)
-    print_beams()
+def post4():
+    do_four(post)
+    pipe('\n')
+
+def grid2():
+    beam2()
+    do_four(post2)
+    beam2()
+
+def grid4():
+    do_twice2(beam2, do_four, post2)
+    beam2()
+
+def grid8():
+    do_twice2(beam4, do_four, post4)
+    do_twice2(beam4, do_four, post4)
+    beam4()
+
+grid8()
